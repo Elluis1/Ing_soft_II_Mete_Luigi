e55c00c364ab858fede1b3b50e792a9e9e35b0d6
Luigi <luigimete54@gmail.com>
2024-03-31 10:19:53 -0300

    punto 4

--- Tp2/chat.py
+++ Tp2/chat.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from openai import OpenAI
 from dotenv import load_dotenv
 
@@ -10,39 +11,54 @@ client = OpenAI(
     api_key=os.environ['OPENAI_API_KEY']
 )
 
+conversation_buffer = [] #almacen de consultas y respuestas
+
+if "--convers" in sys.argv:
+    convers_mode = True
+else:
+    convers_mode = False
+
+def add_to_buffer(query, response):
+    conversation_buffer.append((query, response)) #almacena la consulta y respuesta en el buffer
+
+def get_last_query(query, response):
+    if conversation_buffer:
+        return conversation_buffer[-1][0]
+    else:
+        return ""
+    
+def get_full_query():
+    return " ".join(query for query, _ in conversation_buffer) #Obtiene toda la conversacion
+
 last_prompt = "" #Aca se guarda la ultima consulta
 
 while True:
     try:
-        if last_prompt:
-            prompt = input(f"Edita la última consulta ('{last_prompt}'): ")
+        if convers_mode:
+            prompt = input("Introduce una pregunta (o presiona Ctrl+C para salir): ")
         else:
-            prompt = input("Introduce una consulta (o presiona Ctrl+C para salir): ")
+            prompt = input("Introduce una pregunta: ")
 
         if prompt.strip():
-            last_prompt=prompt
-    except KeyboardInterrupt: #Ctrl + c
-        print('\nSaliendo del programa')
-        break
-    except Exception as e:
-        print('Error al leer la entrada: ', e)
-        continue
+            last_query= get_last_query() if convers_mode else ""
+            full_query = get_full_query() if convers_mode else ""
+            prompt = f"{last_query} {prompt} {full_query}" if convers_mode else prompt
 
-    try:
         completion = client.completions.create(
             model="gpt-3.5-turbo",
-            prompt=last_prompt,
+            prompt=prompt,
             max_tokens=2048
     )
-    except Exception as e:
-        print('Error al obtener la respuesta: ', e)
-        continue
-
-    try:
         print('You: ', prompt)
         print('ChatGPT: ', completion.choices[0].text)
+
+        response = completion.choices[0].text
+
+        if convers_mode:
+            add_to_buffer(prompt, response)
+
+    except KeyboardInterrupt:
+        print("\nSaliendo del programa...")
+        break
     except Exception as e:
-        print('Error al imprimir la respuesta: ', e)
-
-
-
+        print("Error:", e)
